b72ba03537788cd30ea6940fba100f0ed56af917
unknown <florentbr@gmail.com>
2016-03-02 21:26:02 +0100

    Added an option on a TableElement to use some javascript to parse a cell

--- Selenium/Common/TableElement.cs
+++ Selenium/Common/TableElement.cs
@@ -40,52 +40,81 @@ namespace Selenium {
         /// </summary>
         /// <param name="firstRowsToSkip">First row(s) to skip. Ex : 2 will skip the first two rows</param>
         /// <param name="lastRowsToSkip">Last row(s) to skip. Ex : 2 will skip the last two rows</param>
+        /// <param name="map">Optional - Javascript code to scrap each cell. Default: (e)=>e.textContent.trim()</param>
         /// <returns>Excel array</returns>
-        public object[,] Data(int firstRowsToSkip = 0, int lastRowsToSkip = 0) {
-            const string script = "return (function(a){for(var d=[],b=0,g=a.length;b<g;b++){for(var e=a[b].cells,f=[],c=0,h=e.length;c<h;c++)f.push(e[c].textContent);d.push(f)}return d;})(arguments[0].rows);";
-            object response = _session.javascript.Execute(script, new[] { _element }, false);
-            ICollection data = response as ICollection;
-            if (data == null)
-                return null;
-            int dim1 = data.Count;
-            int dim2 = 0;
-            int lastRow = dim1 - lastRowsToSkip;
-            int firstRow = firstRowsToSkip + 1;
-            object[,] table = null;
-            int r = 0, c;
-            foreach (ICollection row in data) {
-                r++;
-                if (r < firstRow || r > lastRow)
-                    continue;
-                if (table == null) {
-                    dim2 = row.Count;
-                    table = (object[,])Array.CreateInstance(typeof(object), new[] { dim1, dim2 }, new[] { 1, 1 });
-                }
-                c = 0;
-                foreach (string cell in row)
-                    table[r, ++c] = (object)cell.Trim();
+        public object[,] Data(int firstRowsToSkip = 0, int lastRowsToSkip = 0, string map = null) {
+            const string JS_SCRAP_TABLE =
+                "var rows=arguments[0].rows, values=[], columns=[];" +
+                "for(var r=0; r<rows.length; r++){" +
+                " for(var c=0, cells=rows[r].cells; c<cells.length; c++)" +
+                "  values.push(map.call(cells[c], cells[c]));" +
+                " columns.push(cells.length);" +
+                "}" +
+                "return {values: values, columns: columns};";
+
+            // build the map function applied to each cell
+            string js_map;
+            if (string.IsNullOrEmpty(map)) {
+                js_map = "var map=function(e){return e.textContent.trim()};";
+            } else if (map.StartsWith("return")) {
+                js_map = "var map=function(){" + map + "};";
+            } else {
+                js_map = "var map=" + map + ";";
             }
+
+            // execute the script
+            var response = (Dictionary)_session.javascript.Execute(js_map + JS_SCRAP_TABLE,
+                                                                   new[] { _element },
+                                                                   false);
+            List values = (List)response["values"];
+            List columns = (List)response["columns"];
+            int rows_count = columns.Count;
+
+            // find the maximum number of columns
+            int cols_count = 0;
+            for (int i = 0; i < rows_count; i++) {
+                if ((int)columns[i] > cols_count)
+                    cols_count = (int)columns[i];
+            }
+
+            // converts to a 2dim array
+            object[,] table = (object[,])Array.CreateInstance(typeof(object),
+                                                              new[] { rows_count, cols_count },
+                                                              new[] { 1, 1 });
+            int j = 0;
+            for (int r = 1; r <= rows_count; r++) {
+                int clen = (int)columns[r - 1];
+                for (int c = 1; c <= clen; c++) {
+                    table[r, c] = values[j++];
+                }
+            }
+
             return table;
         }
 
         /// <summary>
         /// Copies the values to Excel. The target can be an address, a worksheet or a range.
         /// </summary>
-        /// <param name="target">Excel address, worksheet or range.</param>
+        /// <param name="target">Optional - Excel address, worksheet or range. New sheet if null</param>
         /// <param name="clear">Optional - If true, the cells will be cleared first</param>
+        /// <param name="firstRowsToSkip">First row(s) to skip. Ex : 2 will skip the first two rows</param>
+        /// <param name="lastRowsToSkip">Last row(s) to skip. Ex : 2 will skip the last two rows</param>
+        /// <param name="map">Optional - Javascript code to scrap each cell. Default: (e)=>e.textContent.trim()</param>
         /// <example>
         /// Dim lst As New List
         /// lst.Add 43
-        /// lst.ToExcelRange Range("Sheet1!A1")
+        /// lst.ToExcel [Sheet1!A1]
         /// </example>
-        public void ToExcel(object target, bool clear = false) {
+        public void ToExcel(object target = null, bool clear = false
+            , int firstRowsToSkip = 0, int lastRowsToSkip = 0, string map = null) {
+
             IRange range = ExcelExt.GetRange(target);
             if (clear)
                 range.CurrentRegion.Clear();
-            object[,] values = Data();
+            object[,] values = Data(firstRowsToSkip, lastRowsToSkip, map);
             int rowlen = values.GetLength(0);
             int collen = values.GetLength(1);
-            range[values, collen].Value2 = values;
+            range[rowlen, collen].Value2 = values;
         }
 
     }
